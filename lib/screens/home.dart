b726f66c69414a958cbe20e45d52e31e5628f97f
Julio Cesar <julczar21@outlook.com>
2022-05-11 21:23:14 -0300

    feat: created home screen

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,18 +1,32 @@
 import 'package:flutter/material.dart';
 
+import '../widgets/button.dart';
+import '../widgets/custom_input.dart';
+import '../widgets/custom_text.dart';
+
 class Home extends StatelessWidget {
   const Home({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Home')),
-      body: Column(
-        children: const [
-          Center(
-            child: Text('Home'),
-          )
-        ],
+      backgroundColor: const Color.fromRGBO(0, 0, 0, 1),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const CustomText(text: 'Este é um dicionário.', size: 64),
+            const Input(),
+            Button(
+              text: 'Buscar',
+              onPressed: () {
+                Navigator.of(context).pushNamed('/details');
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
